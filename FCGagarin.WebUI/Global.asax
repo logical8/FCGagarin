5592d894d2380b170b566ec941f62190db93ece1
logical8 <alekkireev@gmail.com>
2016-06-25 20:45:17 +0300

    #1 Go Go Go!
    first commit

---
+++ FCGagarin.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FCGagarin.WebUI.MvcApplication" Language="C#" %>
---
+++ FCGagarin.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FCGagarin.WebUI.Startup))]
+namespace FCGagarin.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
